2ddcd28a5d098cdc9c3fd15f918cd05f87ca31ca
Genesis <dmsgktn1685@naver.com>
2023-12-30 21:55:06 +0900

    [Feat] #5 - SocialLoginButtonView 구현

--- TOASTER-iOS/Present/Login/View/SocialLoginButtonView.swift
+++ TOASTER-iOS/Present/Login/View/SocialLoginButtonView.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class SocialLoginButtonView: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Properties
+    
+    var loginType: SocialLoginType
+    
+    // MARK: - Life Cycle
+    
+    init(type: SocialLoginType) {
+        self.loginType = type
+        super.init(frame: .zero)
+        setupSocialButton(type: loginType)
     }
-    */
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private Extensions
+
+extension SocialLoginButtonView {
+    private func setupSocialButton(type: SocialLoginType) {
+        var configuration = UIButton.Configuration.filled()
+        
+        configuration.title = type.title
+        configuration.cornerStyle = .capsule
+        configuration.baseForegroundColor = type.titleColor
+        configuration.baseBackgroundColor = type.backgroundColor
+        
+        self.configuration = configuration
+        clipsToBounds = true
+    }
 }
