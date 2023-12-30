8cca7b524f929ef1c832533c62d10072ffcf56f1
Genesis <dmsgktn1685@naver.com>
2023-12-30 21:38:11 +0900

    [Feat] #5 - SocialLoginButtonView 구현

---
+++ TOASTER-iOS/Present/Login/SocialLoginType.swift
@@ -0,0 +1,8 @@
+//
+//  SocialLoginType.swift
+//  TOASTER-iOS
+//
+//  Created by ParkJunHyuk on 12/30/23.
+//
+
+import Foundation
---
+++ TOASTER-iOS/Present/Login/View/SocialLoginButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  SocialLoginButtonView.swift
+//  TOASTER-iOS
+//
+//  Created by ParkJunHyuk on 12/30/23.
+//
+
+import UIKit
+
+class SocialLoginButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
