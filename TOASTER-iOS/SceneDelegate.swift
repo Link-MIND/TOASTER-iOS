f0f6daafb7a69d533c2ec032d79e38f06f6fe984
Daye Kim <dykim315@naver.com>
2023-12-30 15:45:10 +0900

    [Delete] #2 - 스토리보드 삭제

--- TOASTER-iOS/SceneDelegate.swift
+++ TOASTER-iOS/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let rootViewController = ViewController()
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
