0b59b9821f41b8269a038e29b08df7c0e068a830
Genesis <dmsgktn1685@naver.com>
2023-12-30 21:55:32 +0900

    [Feat] #5 - SocialLoginType 구현

--- TOASTER-iOS/Present/Login/SocialLoginType.swift
+++ TOASTER-iOS/Present/Login/SocialLoginType.swift
@@ -5,4 +5,35 @@
 //  Created by ParkJunHyuk on 12/30/23.
 //
 
-import Foundation
+import UIKit
+
+enum SocialLoginType: String {
+    case kakao, apple
+    
+    var title: String {
+        switch self {
+        case .kakao:
+            return "Kakao로 시작하기"
+        case .apple:
+            return "Apple로 시작하기"
+        }
+    }
+    
+    var titleColor: UIColor {
+        switch self {
+        case .kakao:
+            return UIColor.black
+        case .apple:
+            return UIColor.white
+        }
+    }
+    
+    var backgroundColor: UIColor {
+        switch self {
+        case .kakao:
+            return UIColor.yellow
+        case .apple:
+            return UIColor.black
+        }
+    }
+}
