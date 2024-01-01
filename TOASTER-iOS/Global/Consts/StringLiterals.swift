0753ce97d45ffa67f6cef621105550563b61c0d7
mcrkgus <mcrkgus@naver.com>
2024-01-01 21:50:44 +0900

    [Add] #9 - StringLiterals 추가

---
+++ TOASTER-iOS/Global/Consts/StringLiterals.swift
@@ -0,0 +1,20 @@
+//
+//  StringLiterals.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/01.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum Tabbar {
+            enum title{
+                static let home = "HOME"
+                static let clip = "CLIP"
+                static let timer = "TIMER"
+                static let my = "MY"
+            }
+        }
+}
