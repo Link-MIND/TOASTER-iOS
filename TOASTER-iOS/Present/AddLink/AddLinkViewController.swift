496163158a03a39576f062d10d0d1d097e872195
mcrkgus <mcrkgus@naver.com>
2024-01-04 18:07:31 +0900

    [Add] #21 - AddLinkView 추가

--- TOASTER-iOS/Present/AddLink/AddLinkViewController.swift
+++ TOASTER-iOS/Present/AddLink/AddLinkViewController.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-class AddLinkViewController: UIViewController {
+import SnapKit
+import Then
 
+final class AddLinkViewController: UIViewController {
+    
+    // MARK: - UI Properties
+
+    private let addLinkView = AddLinkView()
+
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// MARK: - Private Extensions
---
+++ TOASTER-iOS/Present/AddLink/View/AddLinkView.swift
@@ -0,0 +1,17 @@
+//
+//  AddLinkView.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/04.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+
+final class AddLinkView: UIView {
+
+    // MARK: - Properties 
+}
