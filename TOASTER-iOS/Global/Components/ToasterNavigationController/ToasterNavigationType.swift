d2d2fa3cde06ece64278de117844b8a3a21ab8f9
Daye Kim <dykim315@naver.com>
2024-01-06 03:29:03 +0900

    [Add] #20 - 주석 추가

--- TOASTER-iOS/Global/Components/ToasterNavigationController/ToasterNavigationType.swift
+++ TOASTER-iOS/Global/Components/ToasterNavigationController/ToasterNavigationType.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 struct ToasterNavigationType {
-    var hasBackButton: Bool
-    var hasRightButton: Bool
+    var hasBackButton: Bool     // backButton 존재 여부
+    var hasRightButton: Bool    // rightButton 존재 여부
 
-    var hasMainImage: Bool
-    var mainImage: UIImage?
-    var mainTitle: String?
+    var hasMainImage: Bool      // mainTitle이 이미지로 존재하는지
+    var mainImage: UIImage?     // mainTitle이 이미지로 존재할 때의 이미지
+    var mainTitle: String?      // mainTitle이 String 값일 때의 String값
     
-    var hasRightButtonImage: Bool
-    var rightButtonImage: UIImage?
-    var rightButtonTitle: String? 
-    var rightButtonAction: () -> Void
+    var hasRightButtonImage: Bool       // rightButton이 이미지로 존재하는지
+    var rightButtonImage: UIImage?      // rightButton이 이미지 버튼일 때의 이미지 값
+    var rightButtonTitle: String?       // rightButton이 String 버튼일 때의 String 값
+    var rightButtonAction: () -> Void   // rightButton의 액션
 }
