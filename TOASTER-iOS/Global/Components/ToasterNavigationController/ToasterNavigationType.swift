66515b0d110d1ce95fae646e138d12d3df22967a
Daye Kim <dykim315@naver.com>
2024-01-06 02:35:20 +0900

    [Add] #20 - NavigationBarType 추가

--- TOASTER-iOS/Global/Components/ToasterNavigationController/ToasterNavigationType.swift
+++ TOASTER-iOS/Global/Components/ToasterNavigationController/ToasterNavigationType.swift
@@ -5,4 +5,17 @@
 //  Created by 김다예 on 1/6/24.
 //
 
-import Foundation
+import UIKit
+
+struct ToasterNavigationType {
+    var hasBackButton: Bool
+    var hasRightButton: Bool
+
+    var hasMainImage: Bool
+    var mainImage: UIImage?
+    var mainTitle: String?
+    
+    var hasRightButtonImage: Bool
+    var rightButtonImage: UIImage?
+    var rightButtonTitle: String? 
+}
