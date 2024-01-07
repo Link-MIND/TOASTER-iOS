79c0633527b55dc6a8c3b5c40a8dbc229096f77b
Genesis <dmsgktn1685@naver.com>
2024-01-07 11:02:51 +0900

    [Feat] #10 - Config 초기 세팅 구현

--- TOASTER-iOS/Global/Resources/Config.swift
+++ TOASTER-iOS/Global/Resources/Config.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum Config {
+    enum Keys {
+        enum Plist {
+            static let kakaoNativeAppKey = "KakaoNativeAppKey"
+            static let baseURL = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found !!!")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    static let kakaoNativeAppKey: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.kakaoNativeAppKey] as? String else {
+            fatalError("KAKAO_NATIVE_APP_KEY is not set in plist for this configuration")
+        }
+        return key
+    }()
+    
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("BASE_URL is not set in plist for this configuration")
+        }
+        return key
+    }()
+}
