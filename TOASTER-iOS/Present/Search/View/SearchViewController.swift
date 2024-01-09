1f9f631b8439320f276ed85970d4c001c1630cb2
Daye Kim <dykim315@naver.com>
2024-01-09 22:13:31 +0900

    [Feat] #34 - Search 플로우 구현

--- TOASTER-iOS/Present/Search/View/SearchViewController.swift
+++ TOASTER-iOS/Present/Search/View/SearchViewController.swift
@@ -14,18 +14,29 @@ final class SearchViewController: UIViewController {
     
     // MARK: - Properties
     
+    private var isSearchButtonHidden: Bool = false {
+        didSet {
+            searchButton.isHidden = isSearchButtonHidden
+            clearButton.isHidden = !isSearchButtonHidden
+        }
+    }
+    
     // MARK: - UI Properties
     
     private let navigationBar: UIView = UIView()
     private let searchTextField: UITextField = UITextField()
     private let backButton: UIButton = UIButton()
-    private let rightButton: UIButton = UIButton()
+    private let searchButton: UIButton = UIButton()
+    private let clearButton: UIButton = UIButton()
+    
+    private let emptyView: SearchEmptyResultView = SearchEmptyResultView()
         
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        hideKeyboard()
         setupStyle()
         setupHierarchy()
         setupLayout()
@@ -35,6 +46,7 @@ final class SearchViewController: UIViewController {
         super.viewWillAppear(animated)
         
         hideNavigationBar()
+        setupEmptyView(isHidden: true)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -56,8 +68,15 @@ private extension SearchViewController {
             $0.setImage(ImageLiterals.Common.arrowLeft, for: .normal)
         }
         
-        rightButton.do {
+        searchButton.do {
             $0.setImage(ImageLiterals.Search.searchIcon, for: .normal)
+            $0.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        }
+        
+        clearButton.do {
+            $0.setImage(ImageLiterals.Search.searchCancle, for: .normal)
+            $0.addTarget(self, action: #selector(clearButtonTapped), for: .touchUpInside)
+            $0.isHidden = true
         }
         
         searchTextField.do {
@@ -65,13 +84,14 @@ private extension SearchViewController {
             $0.addPadding(left: 12, right: 44)
             $0.backgroundColor = .gray50
             $0.placeholder = "검색어를 입력해주세요"
+            $0.delegate = self
         }
     }
     
     func setupHierarchy() {
-        view.addSubviews(navigationBar)
+        view.addSubviews(navigationBar, emptyView)
         navigationBar.addSubviews(backButton, searchTextField)
-        searchTextField.addSubview(rightButton)
+        searchTextField.addSubviews(searchButton, clearButton)
     }
     
     func setupLayout() {
@@ -87,10 +107,12 @@ private extension SearchViewController {
             $0.leading.equalToSuperview().inset(20)
         }
         
-        rightButton.snp.makeConstraints {
-            $0.width.height.equalTo(20)
-            $0.centerY.equalToSuperview()
-            $0.trailing.equalToSuperview().inset(12)
+        [searchButton, clearButton].forEach {
+            $0.snp.makeConstraints {
+                $0.width.height.equalTo(20)
+                $0.centerY.equalToSuperview()
+                $0.trailing.equalToSuperview().inset(12)
+            }
         }
         
         searchTextField.snp.makeConstraints {
@@ -99,5 +121,39 @@ private extension SearchViewController {
             $0.leading.equalTo(backButton.snp.trailing).offset(12)
             $0.trailing.equalToSuperview().inset(20)
         }
+        
+        emptyView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(navigationBar.snp.bottom).offset(view.convertByHeightRatio(176))
+        }
+    }
+    
+    func setupEmptyView(isHidden: Bool) {
+        emptyView.isHidden = isHidden
+    }
+    
+    func fetchSearchResult() {
+        isSearchButtonHidden = true
+        view.endEditing(true)
+        
+        // TODO: - API 호출
+
+    }
+    
+    @objc func searchButtonTapped() {
+        fetchSearchResult()
+    }
+    
+    @objc func clearButtonTapped() {
+        searchTextField.text = nil
+        isSearchButtonHidden = false
+        searchTextField.becomeFirstResponder()
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        fetchSearchResult()
+        return true
     }
 }
