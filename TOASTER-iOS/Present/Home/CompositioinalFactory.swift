a38e0db3107c16db1e7dad278baadec420802eb6
mcrkgus <mcrkgus@naver.com>
2024-01-09 15:48:43 +0900

    [Feat] #32 - CompositionalFactory

---
+++ TOASTER-iOS/Present/Home/CompositioinalFactory.swift
@@ -0,0 +1,143 @@
+//
+//  CompositioinalFactory.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/09.
+//
+
+import UIKit
+
+
+enum CompositionalFactory {
+    static func create() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, environment) -> NSCollectionLayoutSection? in
+            let section: NSCollectionLayoutSection
+            
+            switch sectionNumber {
+            case 0:
+                section = createMainSection()
+            case 1:
+                section = createUserClipSection()
+            case 2:
+                section = createWeeklyLinkSection()
+            case 3:
+                section = createWeeklyRecommendSection()
+            default:
+                section = createMainSection()
+            }
+            return section
+        }
+    }
+    
+    static func createMainSection() -> NSCollectionLayoutSection {
+        let itemFractionalWidthFraction = 1.0
+        let itemInset: CGFloat = 3
+        
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(itemFractionalWidthFraction),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .estimated(134))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        //section.orthogonalScrollingBehavior = .none
+        
+
+        return section
+    }
+    
+    
+    // MARK: - User Clip 에 대한 Layout
+    
+    static func createUserClipSection() -> NSCollectionLayoutSection {
+        let itemFractionalWidthFraction = 1.0 / 2.0 // horizontal 2개의 셀
+        let itemInset: CGFloat = 3
+        
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(itemFractionalWidthFraction),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .estimated(150))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        //section.orthogonalScrollingBehavior = .none
+        
+        section.boundarySupplementaryItems = [
+            NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        ]
+        return section
+    }
+    
+    // MARK: - Weekly Link 에 대한 Layout
+    
+    static func createWeeklyLinkSection() -> NSCollectionLayoutSection {
+        let itemFractionalWidthFraction = 1.0 / 1.0 
+        let itemInset: CGFloat = 3
+        
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(itemFractionalWidthFraction),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(600)
+        )
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 5)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        section.boundarySupplementaryItems = [
+            NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        ]
+        return section
+    }
+    
+    // MARK: - Weekly Recommend 에 대한 Layout
+    
+    static func createWeeklyRecommendSection() -> NSCollectionLayoutSection {
+        let itemFractionalWidthFraction = 1.0 / 3.0 // horizontal 3개의 셀
+        
+        let itemInset: CGFloat = 2.5
+        
+        // Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(itemFractionalWidthFraction),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(150)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: itemInset, leading: itemInset, bottom: itemInset, trailing: itemInset)
+        
+        section.boundarySupplementaryItems = [
+            NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .absolute(50)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        ]
+        return section
+    }
+}
