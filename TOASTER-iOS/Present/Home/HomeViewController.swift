903c9b78744887e9b3caf52c9119f61a475d16cb
mcrkgus <mcrkgus@naver.com>
2024-01-09 23:01:18 +0900

    [Feat] #32 - HomeView 구현

--- TOASTER-iOS/Present/Home/HomeViewController.swift
+++ TOASTER-iOS/Present/Home/HomeViewController.swift
@@ -11,36 +11,154 @@ import SnapKit
 import Then
 
 final class HomeViewController: UIViewController {
-
-
-    
+    private let homeView = HomeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeView.backgroundColor = .toasterBackground
+        setupNavigationBar()
+        setupHierarchy()
+        setupLayout()
+        createCollectionView()
         
     }
-    
-    
+}
 
+extension HomeViewController: UICollectionViewDelegate {}
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 4
+        case 2:
+            return 3
+        case 3:
+            return 9
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCollectionViewCell.className, for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
+            return cell
+        case 1:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserClipCollectionViewCell.className, for: indexPath) as? UserClipCollectionViewCell else { return UICollectionViewCell() }
+            return cell
+        case 2:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeeklyLinkCollectionViewCell.className, for: indexPath) as? WeeklyLinkCollectionViewCell
+            else { return UICollectionViewCell() }
+            return cell
+        default:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeeklyRecommendCollectionViewCell.className, for: indexPath) as? WeeklyRecommendCollectionViewCell
+            else { return UICollectionViewCell() }
+            return cell
+        }
+    }
+    
+    // Header
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        switch indexPath.section {
+//        case 1:
+//            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: UserClipCollectionReusableView.className, for: indexPath) as? UserClipCollectionReusableView
+//            else { return UserClipCollectionReusableView() }
+//            //header.configure()
+//            return header
+//        case 2:
+//            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: WeeklyLinkCollectionReusableView.className, for: indexPath) as? WeeklyLinkCollectionReusableView
+//            else { return WeeklyLinkCollectionReusableView() }
+//            //header.configure()
+//            return header
+//        case 3:
+//            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: WeeklyRecommendCollectionReusableView.className, for: indexPath) as? WeeklyRecommendCollectionReusableView
+//            else { return WeeklyRecommendCollectionReusableView() }
+//            //header.configure()
+//            return header
+//        default:
+//            return UserClipCollectionReusableView()
+//        }
+//    }
+    
+    // Header 크기 지정
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+//        switch section {
+//        case 0:
+//            return CGSize(width: 300, height: 40)
+//        case 1:
+//            return CGSize(width: 300, height: 40)
+//        default:
+//            return CGSize(width: 300, height: 40)
+//        }
+//    }
 }
 
 
 private extension HomeViewController {
     
-    func setupStyle() {
-
-    }
-    
     func setupHierarchy() {
-       
+        view.addSubview(homeView.collectionView)
     }
     
     func setupLayout() {
-
+        homeView.collectionView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
     }
     
-    func setupAddTarget() {
-       
+    func createCollectionView() {
+        let homeCollectionView = homeView.collectionView
+        homeCollectionView.backgroundColor = .toasterBackground
+        homeCollectionView.alwaysBounceVertical = true
+        
+        // Cell register
+        homeCollectionView.register(MainCollectionViewCell.self,
+                          forCellWithReuseIdentifier: MainCollectionViewCell.className)
+        homeCollectionView.register(UserClipCollectionViewCell.self,
+                          forCellWithReuseIdentifier: UserClipCollectionViewCell.className)
+        homeCollectionView.register(WeeklyLinkCollectionViewCell.self,
+                          forCellWithReuseIdentifier: WeeklyLinkCollectionViewCell.className)
+        homeCollectionView.register(WeeklyRecommendCollectionViewCell.self,
+                          forCellWithReuseIdentifier: WeeklyRecommendCollectionViewCell.className)
+        
+        // Header register
+        homeCollectionView.register(UserClipCollectionReusableView.self,
+                          forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                          withReuseIdentifier: UserClipCollectionReusableView.className)
+        
+        homeCollectionView.register(WeeklyLinkCollectionReusableView.self,
+                          forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                          withReuseIdentifier: WeeklyLinkCollectionReusableView.className)
+        
+        homeCollectionView.register(WeeklyRecommendCollectionReusableView.self,
+                          forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                          withReuseIdentifier: WeeklyRecommendCollectionReusableView.className)
+        
+        homeCollectionView.delegate = self
+        homeCollectionView.dataSource = self
     }
-
+    
+    func setupNavigationBar() {
+         let type: ToasterNavigationType = ToasterNavigationType(hasBackButton: false,
+                                                                 hasRightButton: true,
+                                                                 mainTitle: StringOrImageType.image(ImageLiterals.Logo.wordmark),
+                                                                 rightButton: StringOrImageType.image(ImageLiterals.Common.setting),
+                                                                 rightButtonAction: rightButtonTapped)
+                                                                 
+        if let navigationController = navigationController as? ToasterNavigationController {
+              navigationController.setupNavigationBar(forType: type)
+        }
+     }
+    
+    func rightButtonTapped() {
+         // rightButtonAction
+        print("환경설정")
+     }
 }
---
+++ TOASTER-iOS/Present/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/09.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
