ce42e652bfad46dcbdb6c68fe342d3a8f79667e1
mcrkgus <mcrkgus@naver.com>
2024-01-09 17:54:22 +0900

    [Feat] #32 - UserClipCollectionViewCell 구현

--- TOASTER-iOS/Present/Home/View/Cell/UserClipCollectionViewCell.swift
+++ TOASTER-iOS/Present/Home/View/Cell/UserClipCollectionViewCell.swift
@@ -14,4 +14,67 @@ import Then
 
 final class UserClipCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - UI Components
+    
+    private let clipImage = UIImageView()
+    private let titleLabel = UILabel()
+    private let countLabel = UILabel()
+    
+    private let clipStackView = UIStackView()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Make View
+    
+    func setView() {
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+}
+
+
+private extension UserClipCollectionViewCell {
+    
+    func setupStyle() {
+        contentView.backgroundColor = .lightGray
+        clipImage.do {
+            $0.image = UIImage(systemName: "folder")?.withTintColor(.black)
+        }
+        
+        titleLabel.do {
+            $0.text = "전체 클립"
+            $0.font = .boldSystemFont(ofSize: 16)
+            $0.textColor = .black
+        }
+        
+        countLabel.do {
+            $0.text = "n개"
+            $0.font = .boldSystemFont(ofSize: 14)
+        }
+        
+        clipStackView.do {
+            $0.distribution = .equalSpacing
+            $0.axis = .vertical
+        }
+    }
+    
+    func setupHierarchy() {
+        
+        
+    }
+    
+    func setupLayout() {
+        
+    }
 }
