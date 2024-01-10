241ef88f24d59e0ef2826b9113a2bae6580b321d
Genesis <dmsgktn1685@naver.com>
2024-01-10 22:14:42 +0900

    [Feat] #44 - MypageViewController 변경

--- TOASTER-iOS/Present/Mypage/MypageViewController.swift
+++ TOASTER-iOS/Present/Mypage/MypageViewController.swift
@@ -7,23 +7,74 @@
 
 import UIKit
 
-class MypageViewController: UIViewController {
+import SnapKit
+import Then
 
+final class MypageViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - UI Properties
+
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupStyle()
+        setupHierarchy()
+//        setupLayout()
+//        setupDelegate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+        setupNavigationBar()
     }
-    */
+}
+//// MARK: - Networks
+//
+//extension MypageViewController {
+//    func fetchMain() {
+//        <#code#>
+//    }
+//}
+// MARK: - Private Extensions
 
+private extension MypageViewController {
+    func setupStyle() {
+        view.backgroundColor = .toasterBackground
+    }
+    
+    func setupHierarchy() {
+        view.addSubviews()
+    }
+    
+    func setupLayout() {
+        <#code#>
+    }
+    
+    func setupDelegate() {
+        <#code#>
+    }
+    
+    func setupNavigationBar() {
+        let type: ToasterNavigationType = ToasterNavigationType(hasBackButton: false,
+                                                                hasRightButton: true,
+                                                                mainTitle: StringOrImageType.string(StringLiterals.Tabbar.Title.my),
+                                                                rightButton: StringOrImageType.image(ImageLiterals.Common.setting),
+                                                                rightButtonAction: settingButtonTapped)
+        
+        if let navigationController = navigationController as? ToasterNavigationController {
+            navigationController.setupNavigationBar(forType: type)
+        }
+    }
+        
+    func settingButtonTapped() {
+        // 편집 버튼 클릭 시 로직
+    }
 }
