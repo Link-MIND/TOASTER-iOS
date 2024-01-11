671fad790e8e98981fc21cf09295662ce98bf389
mcrkgus <mcrkgus@naver.com>
2024-01-11 10:21:08 +0900

    [Add] #32 - 에셋 추가

---
+++ TOASTER-iOS/Present/Home/View/Cell/UserClipEmptyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserClipEmptyCollectionViewCell.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/11.
+//
+
+import UIKit
+
+class UserClipEmptyCollectionViewCell: UICollectionViewCell {
+    
+}
