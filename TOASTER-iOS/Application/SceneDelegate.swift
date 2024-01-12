13e76c9e01cf979ce218d52b10aa8f55f7223584
Daye Kim <dykim315@naver.com>
2024-01-13 02:58:18 +0900

    [Chore] #48 - SceneDelegate 잘못 수정

--- TOASTER-iOS/Application/SceneDelegate.swift
+++ TOASTER-iOS/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        let rootViewController = RemindSelectClipViewController()
+        let rootViewController = ViewController()
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
