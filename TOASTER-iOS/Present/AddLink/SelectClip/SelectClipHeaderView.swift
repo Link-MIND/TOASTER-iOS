c56536a2318fa322d918ac2fac3c3d467bfdb12a
mcrkgus <mcrkgus@naver.com>
2024-01-15 08:57:51 +0900

    [Feat] #71 - SelectClipHeader

---
+++ TOASTER-iOS/Present/AddLink/SelectClip/SelectClipHeaderView.swift
@@ -0,0 +1,106 @@
+//
+//  SelectClipHeaderView.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/15.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+protocol SelectClipHeaderViewlDelegate: AnyObject {
+    func addClipCellTapped()
+}
+
+final class SelectClipHeaderView: UICollectionReusableView {
+    
+    // MARK: - Properties
+
+    var clipCount = 3
+    
+    weak var selectClipHeaderViewDelegate: SelectClipHeaderViewlDelegate?
+
+    // MARK: - UI Properties
+    
+    private let desciptLabel = UILabel()
+    private let totalCountLabel = UILabel()
+    private let addClipButton = UIButton()
+    
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        
+//        setupStyle()
+//        setupHierarchy()
+//        setupLayout()
+
+    }
+    
+    func setupView() {
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+// MARK: - Private Extension
+
+private extension SelectClipHeaderView {
+    func setupStyle() {
+        backgroundColor = .toasterBackground
+    
+        desciptLabel.do {
+            $0.text = "클립을 선택해주세요"
+            $0.textColor = .black900
+            $0.font = .suitMedium(size: 18)
+        }
+        
+        totalCountLabel.do {
+            // viewModel.clipData.count
+            $0.text = "전체 (\(clipCount))"
+            $0.textColor = .gray500
+            $0.font = .suitBold(size: 12)
+        }
+        
+        addClipButton.do {
+            $0.setImage(ImageLiterals.Clip.orangeplus, for: .normal)
+            $0.setTitle(StringLiterals.Clip.Title.addClip, for: .normal)
+            $0.setTitleColor(.toasterPrimary, for: .normal)
+            $0.titleLabel?.font = .suitBold(size: 12)
+            $0.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        }
+    }
+    
+    func setupHierarchy() {
+        addSubviews(desciptLabel, totalCountLabel, addClipButton)
+    }
+    
+    func setupLayout() {
+        desciptLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaInsets)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        totalCountLabel.snp.makeConstraints  {
+            $0.top.equalTo(desciptLabel.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        addClipButton.snp.makeConstraints {
+            $0.top.equalTo(desciptLabel.snp.bottom).offset(15)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+
+    @objc func buttonTapped() {
+        selectClipHeaderViewDelegate?.addClipCellTapped()
+    }
+}
