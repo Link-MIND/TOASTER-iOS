09da5570efa7d8c721789c7dab08c603d73e957f
Daye Kim <dykim315@naver.com>
2024-01-15 07:52:23 +0900

    [Chore] #68 - DTO 수정

--- TOASTER-iOS/Network/Search/DTO/GetMainPageSearchResponseDTO.swift
+++ TOASTER-iOS/Network/Search/DTO/GetMainPageSearchResponseDTO.swift
@@ -14,6 +14,7 @@ struct GetMainPageSearchResponseDTO: Codable {
 }
 
 struct GetMainPageSearchResponseData: Codable {
+    let keyword: String
     let toasts: [Toast]
     let categories: [Category]
 }
