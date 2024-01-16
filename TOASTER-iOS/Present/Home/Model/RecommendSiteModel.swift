97d4cea799ca37bbdf3ff5e8756c82378cde868f
mcrkgus <mcrkgus@naver.com>
2024-01-17 00:35:47 +0900

    [Feat] #82 - RecommendSite Model 추가

---
+++ TOASTER-iOS/Present/Home/Model/RecommendSiteModel.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendSiteModel.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 2024/01/16.
+//
+
+import Foundation
+
+struct RecommendSiteModel {
+    let siteId: Int
+    let siteTitle: String?
+    let siteUrl: String?
+    let siteImg: String?
+    let siteSub: String?
+}
