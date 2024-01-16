60fd1a1e597814e95ea65ac85fdbf92e9c21886d
Daye Kim <dykim315@naver.com>
2024-01-16 10:38:18 +0900

    [Feat] #84 - Remind 조회 API 연결

--- TOASTER-iOS/Present/Remind/ViewModel/RemindViewModel.swift
+++ TOASTER-iOS/Present/Remind/ViewModel/RemindViewModel.swift
@@ -16,10 +16,15 @@ final class RemindViewModel {
     
     // MARK: - Data
 
-    var timerData: RemindModel = RemindModel.fetchDummyModel() {
+    var timerData: RemindModel = RemindModel(completeTimerModelList: [],
+                                             waitTimerModelList: []) {
         didSet {
             dataChangeAction?()
         }
+    }
+    
+    init() {
+        fetchTimerData()
     }
 }
 
@@ -28,5 +33,31 @@ final class RemindViewModel {
 extension RemindViewModel {
     func setupDataChangeAction(changeAction: @escaping DataChangeAction) {
         dataChangeAction = changeAction
+    }
+    
+    func fetchTimerData() {
+        NetworkService.shared.timerService.getTimerMainpage { result in
+            switch result {
+            case .success(let response):
+                var completedList: [CompleteTimerModel] = []
+                response?.data.completedTimerList.forEach {
+                    completedList.append(CompleteTimerModel(id: $0.timerId,
+                                                            remindDay: $0.remindDate,
+                                                            remindTime: $0.remindTime,
+                                                            clipName: $0.comment))
+                }
+                var waitList: [WaitTimerModel] = []
+                response?.data.waitingTimerList.forEach {
+                    waitList.append(WaitTimerModel(id: $0.timerId,
+                                                   clipName: $0.comment,
+                                                   remindDay: $0.remindDates,
+                                                   remindTime: $0.remindTime,
+                                                   isEnable: $0.isAlarm))
+                }
+                self.timerData = RemindModel(completeTimerModelList: completedList,
+                                             waitTimerModelList: waitList)
+            default: break
+            }
+        }
     }
 }
