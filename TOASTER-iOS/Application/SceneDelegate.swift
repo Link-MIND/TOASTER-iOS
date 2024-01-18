7c7b9db4b6e41a8e07f454d71d213b6a9cae3c6e
Daye Kim <dykim315@naver.com>
2024-01-18 17:13:51 +0900

    [Chore] #119 - 로그아웃 시 링크 저장 동작 해제

--- TOASTER-iOS/Application/SceneDelegate.swift
+++ TOASTER-iOS/Application/SceneDelegate.swift
@@ -11,14 +11,14 @@ import KakaoSDKAuth
 import KakaoSDKCommon
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
+        
         var rootViewController = appDelegate.isLogin ? TabBarController() : LoginViewController()
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -29,42 +29,48 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
         if UIPasteboard.general.string != nil {
-            if let rootViewController = window?.rootViewController as? ToasterNavigationController {
-                let addLinkViewController = AddLinkViewController()
-                rootViewController.pushViewController(addLinkViewController, animated: true)
-                addLinkViewController.embedURL(url: UIPasteboard.general.string ?? "")
+            if appDelegate.isLogin {
+                if let rootViewController = window?.rootViewController as? ToasterNavigationController {
+                    let addLinkViewController = AddLinkViewController()
+                    rootViewController.pushViewController(addLinkViewController, animated: true)
+                    addLinkViewController.embedURL(url: UIPasteboard.general.string ?? "")
+                    UIPasteboard.general.string = nil
+                }
             }
         }
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
+    
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
             if (AuthApi.isKakaoTalkLoginUrl(url)) {
