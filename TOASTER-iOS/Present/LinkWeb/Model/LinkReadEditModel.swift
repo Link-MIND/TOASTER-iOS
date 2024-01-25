0d80d241b852ad3f701163d294f8d7db42e6c75b
MinjaeLee <2alswo7@khu.ac.kr>
2024-01-25 15:34:38 +0900

    [Refactor] #150 - 링크뷰 DTO/모델 분리

---
+++ TOASTER-iOS/Present/LinkWeb/Model/LinkReadEditModel.swift
@@ -0,0 +1,13 @@
+//
+//  LinkReadEditModel.swift
+//  TOASTER-iOS
+//
+//  Created by 민 on 1/25/24.
+//
+
+import Foundation
+
+struct LinkReadEditModel {
+    let toastId: Int
+    let isRead: Bool
+}
