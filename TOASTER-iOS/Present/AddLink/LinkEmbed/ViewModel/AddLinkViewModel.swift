d34334831fc9b79d0355ad3e6250ca1f19d6617c
mcrkgus <mcrkgus@naver.com>
2024-09-24 14:51:06 +0900

    [Chore] #201 - Change to AddLinkViewModel Input Output structure

--- TOASTER-iOS/Present/AddLink/LinkEmbed/ViewModel/AddLinkViewModel.swift
+++ TOASTER-iOS/Present/AddLink/LinkEmbed/ViewModel/AddLinkViewModel.swift
@@ -8,21 +8,61 @@
 import UIKit
 
 protocol AddLinkViewModelInputs {
-    // Textfield Text
-}
+    func embedLinkText(_ text: String)}
 
 protocol AddLinkViewModelOutputs {
-    // Next Button Enabled
-    // Next Button BackgroundColor
-    // Textfield BorderColor
-    // Textfield Error Message - Link effectiveness
+    var isNextButtonEnabled: Bool { get }
+    var nextButtonBackgroundColor: UIColor { get }
+    var textFieldBorderColor: UIColor { get }
+    var linkEffectivenessMessage: String? { get }
 }
 
 protocol AddLinkViewModelType {
     var inputs: AddLinkViewModelInputs { get }
-    var outputs: AddLinkViewModelOutpus { get }
+    var outputs: AddLinkViewModelOutputs { get }
 }
 
-final class AddLinkViewModel {
+final class AddLinkViewModel: AddLinkViewModelType, AddLinkViewModelInputs, AddLinkViewModelOutputs {
     
+    // Input
+    private var embedLink: String = "" {
+        didSet {
+            updateOutputs()
+        }
+    }
+    
+    // Output
+    var isNextButtonEnabled: Bool
+    var nextButtonBackgroundColor: UIColor
+    var textFieldBorderColor: UIColor
+    var linkEffectivenessMessage: String?
+    
+    init() {
+        self.isNextButtonEnabled = false
+        self.nextButtonBackgroundColor = .gray200
+        self.textFieldBorderColor = .clear
+        self.linkEffectivenessMessage = nil
+    }
+    
+    func embedLinkText(_ text: String) {
+        embedLink = text
+    }
+
+    var inputs: AddLinkViewModelInputs { return self }
+    var outputs: AddLinkViewModelOutputs { return self }
+}
+
+private extension AddLinkViewModel {
+    func updateOutputs() {
+        let isValid = isValidURL(embedLink)
+        isNextButtonEnabled = !embedLink.isEmpty && isValid
+        nextButtonBackgroundColor = isNextButtonEnabled ? .black850 : .gray200
+        textFieldBorderColor = isValid ? .clear : UIColor.toasterError
+        linkEffectivenessMessage = isValid ? nil : (embedLink.isEmpty ? "링크를 입력해주세요" : "유효하지 않은 형식의 링크입니다.")
+    }
+    
+    func isValidURL(_ urlString: String) -> Bool {
+        guard let url = URL(string: urlString) else { return false }
+        return UIApplication.shared.canOpenURL(url)
+    }
 }
